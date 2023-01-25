b747fb58b0b1e680a77970c45be054948e04661c
정현규 <gusrb0511@gmail.com>
2023-01-25 23:07:23 +0900

    temp 230125

--- src/App.js
+++ src/App.js
@@ -4,27 +4,12 @@ import { Navbar, Container, Nav, Table} from 'react-bootstrap'
 import { db } from "./firebase.js";
 import { onValue, ref } from "firebase/database";
 import { useState, useEffect } from 'react';
-const TEST_DATA=[
-  {
-    "timeStamp": "20220614-113330",
-    "uid": "admin",
-    "url": "url1"
-  },
-  {
-    "timeStamp": "20220614-113332",
-    "uid": "admin",
-    "url": "url2"
-  },
-  {
-    "timeStamp": "20220614-113333",
-    "uid": "admin",
-    "url": "url3"
-  }]
+import { useSelector } from 'react-redux';
+
 function App() {
-  let [data,setData] = useState(TEST_DATA);
+  let state = useSelector((state)=>state)
 
   useEffect(() => {
-    ReadDb();
   }, []);
 
   return (
@@ -50,51 +35,16 @@ function App() {
           </tr>
         </thead>
         <tbody id="tbody">
+          <tr>
+            <th>#</th>
+            <th>{state.fsdata[0].timeStamp}</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
         </tbody>
       </Table>
     </div>
     
   );
-}
-var tNo = 0;
-var tbody = document.getElementById('tbody');
-function ReadDb(){
-  const userdb = ref(db, "Users/");
-    onValue(userdb, (snapshot) => {
-      let urls = [];
-      
-      snapshot.forEach(childSnapshot => {
-        urls.push(childSnapshot.val());
-      });
-      AddAllItemsToTable(urls);
-      console.log(urls)
-  });
-}
-function AddAllItemsToTable(TheUrl){
-  tNo=0;
-  tbody.innerHTML="";
-  TheUrl.forEach(element => {
-    AddItemToTable(element.timeStamp, element.uid, element.url);
-  })
-}
-function AddItemToTable(time,email,url){
-  let trow = document.createElement("tr");
-  let td1 = document.createElement('td');
-  let td2 = document.createElement('td');
-  // let td3 = document.createElement('td');
-  let td4 = document.createElement('td');
-
-  td1.innerHTML=++tNo;
-  td2.innerHTML=time;
-  // td3.innerHTML=email;
-  td4.innerHTML=`<img src = "${url}" class="mw-100">`;
-  console.log(td4.innerHTML);
-
-  trow.appendChild(td1);
-  trow.appendChild(td2);
-  // trow.appendChild(td3);
-  trow.appendChild(td4);
-
-  tbody.appendChild(trow);
 }
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,15 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { db } from "./firebase.js";
+import { onValue, ref } from "firebase/database";
 
 // const userdb = ref(db, "Users/");
-//     onValue(userdb, (snapshot) => {
-//       let urls = [];
-      
-//       snapshot.forEach(childSnapshot => {
-//         urls.push(childSnapshot.val());
-//       });
-//       AddAllItemsToTable(urls);
+// onValue(userdb, (snapshot) => {
+//   let urls = [];
+//   snapshot.forEach(childSnapshot => {
+//     urls.push(childSnapshot.val());
 //   });
+// });
+let fsdata = createSlice({
+  name:'cart',
+  initialState:[
+    {
+      "timeStamp": "20220614-113330",
+      "uid": "admin",
+      "url": "url1"
+    },
+    {
+      "timeStamp": "20220614-113332",
+      "uid": "admin",
+      "url": "url2"
+    },
+    {
+      "timeStamp": "20220614-113333",
+      "uid": "admin",
+      "url": "url3"
+    }],
+    reducer:{
+      addCount(state){
+        state[0].url++
+      }
+    }
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: {
+    fsdata:fsdata.reducer
+   }
 }) 

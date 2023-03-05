cbb5cf608ee5deed60a4ea3d6a63d0e476876046
정현규 <gusrb0511@gmail.com>
2023-03-05 17:59:51 +0900

    change protected routes using nested routes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import NavigationBar from './containers/navbar/NavigationBar'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useNavigate } from 'react-router-dom'
-import TableScreen from './containers/table/TableScreen'
 import Login from './containers/login/Login'
 import SignUp from './containers/signup/SignUp'
 import { Routes,Route } from 'react-router-dom'
-import ProtectedRoute from './containers/protectedRoutes/ProtectedRoute'
+import PrivateRoutes from './containers/privateRoutes/PrivateRoutes'
+import { Suspense, lazy } from 'react';
+
+const TableScreen = lazy(() => import('./containers/table/TableScreen'));
 
 function App() {
   let navigate = useNavigate();
   return (
     <div className="App">
       <NavigationBar navigate={navigate}  />
-      <Routes>
-        <Route path = '/목록' element={ <ProtectedRoute><TableScreen/></ProtectedRoute> }/>
-        <Route path = '/Login' element={ <Login/> }/>
-        <Route path = '/Signup' element={ <SignUp/> }/>
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route element={ <PrivateRoutes />}>
+            <Route path = '/목록' element={ <TableScreen/> } />
+          </Route>
+          <Route path = '/Login' element={ <Login/> }/>
+          <Route path = '/Signup' element={ <SignUp/> }/>
+        </Routes>
+      </Suspense>
+
     </div>
     
   );
 }
-export default App;
+export default App; 

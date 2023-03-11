f0638050a585315d3cb7702ed200dee097d10e8a
정현규 <gusrb0511@gmail.com>
2023-03-11 14:20:33 +0900

    delete console log

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -21,7 +21,6 @@ const Login = () => {
             
             const user = userCredential.user;
 
-            console.log(user);
             setLoading(false);
             alert('성공적으로 로그인했습니다');
             navigate('/목록');
@@ -37,7 +36,6 @@ const Login = () => {
           const provider = new GoogleAuthProvider();
           const userCredential = await signInWithPopup(auth, provider);
           const user = userCredential.user;
-          console.log(user);
           setLoading(false);
           alert('성공적으로 로그인했습니다');
           navigate('/목록');

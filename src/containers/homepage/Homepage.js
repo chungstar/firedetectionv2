7011d83989a53cf43e7cb992fc1018eefed92202
정현규 <gusrb0511@gmail.com>
2023-03-12 23:12:06 +0900

    0312 change commit

--- src/containers/homepage/Homepage.js
+++ src/containers/homepage/Homepage.js
@@ -4,51 +4,29 @@ import styles from './styles.module.css';
 import { useTrail, animated } from '@react-spring/web'
 import ReactPlayer from 'react-player'
 import 땃쥐 from '../../img/1663106549.gif'
-function Page({ offset, onClick }) {
-  return (
-    <>
-      <ParallaxLayer sticky={{ start : 0.9, end : 3.5 }}>
-        <img src={땃쥐}/>
-      </ParallaxLayer>
-      <ParallaxLayer offset={0}>
-        <span>      
-          1
-        </span>
-      </ParallaxLayer>
-      <ParallaxLayer offset={1}>
-        <span>2</span>
-      </ParallaxLayer>
-      <ParallaxLayer offset={2}>
-        <div className={styles.container}>
-          <ReactPlayer className={styles.video} url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-        </div>
-      </ParallaxLayer>
-      <ParallaxLayer offset={3}>
-        <div className={styles.container}>
-          <ReactPlayer className={styles.video} url='https://www.youtube.com/watch?v=Ofo64-esJ1k' />
-        </div>
-      </ParallaxLayer>
-    </>
-  );
-}
+
 export default function Homepage() {
-  const parallax = useRef(null);
-
-  function scroll(to) {
-    if (parallax.current) {
-      parallax.current.scrollTo(to);
-    }
-  }
-
   return (
-    <div style={{ background: '#dfdfdf' }}>
-      <Parallax ref={parallax} pages={4}>
-        <Page offset={0} onClick={() => scroll(1)} />
-        <Page offset={1} onClick={() => scroll(2)} />
-        <Page offset={2} onClick={() => scroll(3)} />
-        <Page offset={3} onClick={() => scroll(0)} />
+    <div>
+      <Parallax className={styles.container} pages={3}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          className={`${styles.gradient}`}
+          style={{backgroundImage: `url('${땃쥐}')`, backgroundSize: 'cover',}}>
+          <div>      
+            <h1>딥러닝을 통한 화재감지</h1>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1}>
+          <div>2</div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2}>
+          <div>
+            <ReactPlayer className={styles.video} url='https://www.youtube.com/watch?v=Ofo64-esJ1k' />
+          </div>
+        </ParallaxLayer>
       </Parallax>
-
     </div>
   );
 }

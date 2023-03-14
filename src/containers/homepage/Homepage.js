37842c0e6ad5790a35b4ca541dd41c15d1f3a6fa
정현규 <gusrb0511@gmail.com>
2023-03-14 23:18:39 +0900

    0314 temp

--- src/containers/homepage/Homepage.js
+++ src/containers/homepage/Homepage.js
@@ -3,11 +3,11 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import styles from './styles.module.css';
 import { useTrail, animated } from '@react-spring/web'
 import ReactPlayer from 'react-player'
-import 땃쥐 from '../../img/1663106549.gif'
+import 화재감지1 from '../../img/화재감지1.gif'
 
 export default function Homepage() {
 
-  const trails = useTrail(2, {
+  const trails = useTrail(1, {
     from: { opacity: 0 },
     to: { opacity: 1 },
   })
@@ -17,22 +17,45 @@ export default function Homepage() {
       <Parallax className={styles.container} pages={3}>
         <ParallaxLayer
           offset={0}
-          speed={1}
-          style={{backgroundImage: `url('${땃쥐}')`, backgroundSize: 'cover',}}>
-          <div className={`${styles.gradient}`}>      
-            <h1 className='mt-5 text-light'>딥러닝을 통한 화재감지</h1>
+          speed={0.1}
+          factor={1.3}
+          style={{
+            backgroundColor:'black',
+            backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.2)), url(${화재감지1})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={1}
+          style={{
+            backgroundColor:'#6C7A89'
+          }}>
+          <div>
+            react,react-spring,react-router,react-bootstrap,
+
+            firebase
+
+            python
           </div>
         </ParallaxLayer>
-        <ParallaxLayer offset={1}>
-        <div>
-          {trails.map(props => (
-          <animated.div style={props}>Hello World</animated.div>
-        ))}
-        </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={2}>
+
+        <ParallaxLayer
+          offset={2}
+          style={{
+            backgroundColor:'black'
+          }}>
           <div>
             <ReactPlayer className={styles.video} url='https://www.youtube.com/watch?v=Ofo64-esJ1k' />
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.1}
+          speed={0.5}
+          factor={1}>
+          <div>
+            <div className='fs-1 fw-semibold text-light'>딥러닝을 통한 화재감지</div>
           </div>
         </ParallaxLayer>
       </Parallax>

1798a2b815086fd47f82f5dc0c0d90e0e5fb021b
정현규 <gusrb0511@gmail.com>
2023-03-15 22:59:18 +0900

    trail

--- src/containers/homepage/Homepage.js
+++ src/containers/homepage/Homepage.js
@@ -1,16 +1,33 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import styles from './styles.module.css';
-import { useTrail, animated } from '@react-spring/web'
+import { useTrail, animated, a } from '@react-spring/web'
 import ReactPlayer from 'react-player'
 import 화재감지1 from '../../img/화재감지1.gif'
+import { Container, Row, Col } from 'react-bootstrap';
+const Trail = ({ open, children }) => {
+  const items = React.Children.toArray(children);
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? 110 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  });
+  return (
+    <div>
+      {trail.map(({ height, ...style }, index) => (
+        <a.div key={index} className={styles.trailsText} style={style}>
+          <a.div style={{ height }}>{items[index]}</a.div>
+        </a.div>
+      ))}
+    </div>
+  );
+};
 
 export default function Homepage() {
 
-  const trails = useTrail(1, {
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-  })
+  const [open, set] = useState(true);
 
   return (
     <div>
@@ -20,7 +37,7 @@ export default function Homepage() {
           speed={0.1}
           factor={1.3}
           style={{
-            backgroundColor:'black',
+            backgroundColor: 'black',
             backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.2)), url(${화재감지1})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
@@ -29,21 +46,32 @@ export default function Homepage() {
         <ParallaxLayer
           offset={1}
           style={{
-            backgroundColor:'#6C7A89'
+            backgroundColor: 'white'
           }}>
-          <div>
-            react,react-spring,react-router,react-bootstrap,
-
-            firebase
-
-            python
-          </div>
+          <Container className={styles.container} onClick={() => set((state) => !state)}>
+          <Row>
+            <Col>
+            <Trail open={open}>
+              <div>hi</div>
+              <div>hi</div>
+              <div>hi</div>
+            </Trail>
+            </Col>
+            <Col>
+            <Trail open={open}>
+              <div>hi</div>
+              <div>hi</div>
+              <div>hi</div>
+            </Trail>
+            </Col>
+          </Row>
+          </Container>
         </ParallaxLayer>
 
         <ParallaxLayer
           offset={2}
           style={{
-            backgroundColor:'black'
+            backgroundColor: 'black'
           }}>
           <div>
             <ReactPlayer className={styles.video} url='https://www.youtube.com/watch?v=Ofo64-esJ1k' />

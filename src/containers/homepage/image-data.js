77faabc8e338025609d8d0123f15e983d051f8e3
정현규 <gusrb0511@gmail.com>
2023-05-11 01:21:19 +0900

    img url test

--- src/containers/homepage/image-data.js
+++ src/containers/homepage/image-data.js
@@ -2,7 +2,7 @@ import 화재감지1 from '../../img/화재감지1.gif'
 
 export const images = [
     화재감지1,
-    "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
+    "https://user-images.githubusercontent.com/58381211/204461922-5ab03b54-6948-4711-b476-3cf7064015a2.png",
     "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png"
   ];
   
